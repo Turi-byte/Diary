58c97f925f7811a784d1a910f717f6fe6569ef34
Turi-byte <breldanmuturi001@gmail.com>
2019-08-10 11:53:27 +0300

    Create the entry component and moves the entry logic from the app component to the entry component

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { Entry } from './entry'
-
+import { Component, OnInit } from '@angular/core';
+import { Entry } from '../entry'
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-entry',
+  templateUrl: './entry.component.html',
+  styleUrls: ['./entry.component.css']
 })
-export class AppComponent {
+export class EntryComponent implements OnInit {
   entries:Entry = [
     {id:1, title:'Going for movies'},
     {id:2, title:'Ate Lisagna'},
@@ -15,4 +14,9 @@ export class AppComponent {
     {id:5, title:'Bike riding in Limuru'},
     {id:6, title:'Presented my project at Moringa'},
   ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
